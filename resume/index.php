145e9efa7fc7b04ccc59427db602eafb8cda01fb
tsidel <jeana@jeanaclark.org>
2012-05-14 00:29:06 -0400

    beginning of resume

---
+++ resume/index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+
+<!--[if lt IE 7]> <html class="no-js ie6" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8" lang="en"> <![endif]-->
+
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
+<head>
+  <meta charset="utf-8">
+
+  <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title>J. Clark &raquo; Resume</title>
+  <meta name="description" content="Resume">
+  <meta name="author" content="J. Clark">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/fonts.css" type="text/css" media="screen" title="no title" charset="utf-8">
+
+  <script src="/js/modernizr-2.5.3-min.js"></script>
+
+  <!-- Later add google analytics -->
+
+</head>
+
+<body>
+  <header>
+j.clark
+e: j [at]jclark[dot]org //
+w: <a href="http://www.jeanaclark.org">http://www.jeanaclark.org</a> //
+t: <a href="http://www.twitter.com/jeana_with_a_j">@jeana_with_a_j</a>
+
+  </header>
+
+
+  <section id="about">
+I work at the intersection of Design, Code, and Content to create community in digital space.
+
++ Creative designer with over 10 years of experience working on websites and printed materials in corporate, independent, non-profit and higher education environments.
+
++ Graphic design skills including web and print design, logo and brand development, and publication design.
+
++ Extensive programming experience coding HTML, CSS, [less extensive]PHP, JavasScript, ASP, mySQL
+
++ Custom programming, theming and administration of content management systems.
+
++ Storyteller/content curator/Social Media . . . 
+
+
+
+
+  </section>
+
+
+
+
+  <footer>
+
+
+  </footer>
+
+</body>
+</html>
