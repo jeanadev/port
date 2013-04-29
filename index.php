06872458b100f3cbbc3c758c54779c60a06df094
tsidel <jeana@jeanaclark.org>
2013-04-29 00:28:32 -0400

    added SterlingSilver to the portfolio

--- index.php
+++ index.php
@@ -4,15 +4,18 @@
 
 		<h3 style="margin:13px;letter-spacing:1px;">RECENT WORK</h3>
     <div id="port">
-    
-      <div class="p1"><a href="img/ack-web.png" title="Beyond Grep / ack :: Front End Web Development :: HTML &amp; CSS" class="thickbox"></a><p>WEB :: HTML &amp; CSS</p></div>
-   
-      <div class="p3"><a href="img/h2oMaestroVan.jpg" title="H2O Maestro Logo :: Designed for The Serif Group" class="thickbox"></a><p>LOGO :: Illustration</p></div>
+		<div class="p6"><a href="img/web-GattonUnited.png" title="Gatton College of Business &amp; Economics :: Fundraising Website :: HTML, CSS, &amp; Classic ASP" class="thickbox"></a><p>WEB :: HTML, CSS, Classic ASP</p></div>
+
+        <div class="p4"><a href="img/logo-JuiceBib.png" title="JuiceBib Identity, Logo, and Collateral" class="thickbox"></a><p>LOGO :: Illustration</p></div>
+ 
       <div class="p2"><a href="img/web-GattonPartner.jpg" title="Gatton College of Business &amp; Economics :: Corporate Partners Website :: HTML &amp; CSS" class="thickbox"></a><p>Web :: HTML &amp; CSS</p></div>
 
-      <div class="p4"><a href="img/logo-JuiceBib.png" title="JuiceBib Identity, Logo, and Collateral" class="thickbox"></a><p>LOGO :: Illustration</p></div>
-      <div class="p5"><a href="img/follettbound.png" title="Follett Library Resources :: Product Logo" class="thickbox"></a><p>LOGO :: ILLUSTRATION</p></div>
-      <div class="p6"><a href="img/web-GattonUnited.png" title="Gatton College of Business &amp; Economics :: Fundraising Website :: HTML, CSS, &amp; Classic ASP" class="thickbox"></a><p>WEB :: HTML, CSS, Classic ASP</p></div>
+<!--      <div class="p3"><a href="img/h2oMaestroVan.jpg" title="H2O Maestro Logo :: Designed for The Serif Group" class="thickbox"></a><p>LOGO :: Illustration</p></div> -->
+
+      <div class="p3"><a href="img/web-InboxOrange.png" title="InboxOrange :: Web design refresh, new look &amp; feel, icons, and logo typeface change. Designed for The Serif Group" class="thickbox"></a><p>Web :: Design, &amp; icon illustration</p></div>
+
+      <div class="p5"><a href="img/logo-SterlingSilver.png" title="SterlingSilver Photography :: Logo" class="thickbox"></a><p>LOGO :: ILLUSTRATION</p></div>
+      <div class="p1"><a href="img/ack-web.png" title="Beyond Grep / ack :: Front End Web Development :: HTML &amp; CSS" class="thickbox"></a><p>WEB :: HTML &amp; CSS</p></div>
     </div>
                 <div style="clear:both;"></div>
 
