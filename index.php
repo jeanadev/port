f4e0a4bd84ea07d2399e2785530a1315eafd8a17
tsidel <jeana@jeanaclark.org>
2013-04-28 23:39:06 -0400

    added Gatton Partner images and css to portfolio

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
       <div class="p1"><a href="img/ack-web.png" title="Beyond Grep / ack :: Front End Web Development :: HTML &amp; CSS" class="thickbox"></a><p>WEB :: HTML &amp; CSS</p></div>
    
       <div class="p3"><a href="img/h2oMaestroVan.jpg" title="H2O Maestro Logo :: Designed for The Serif Group" class="thickbox"></a><p>LOGO :: Illustration</p></div>
-      <div class="p2"><a href="img/asbury-minconf.png" title="Asbury Theological Seminary :: Ministry Conference Brochure :: Conference Identity &amp; Collateral Design" class="thickbox"></a><p>Web :: HTML & CSS</p></div>
+      <div class="p2"><a href="img/web-GattonPartner.jpg" title="Gatton College of Business &amp; Economics :: Corporate Partners Website :: HTML &amp; CSS" class="thickbox"></a><p>Web :: HTML &amp; CSS</p></div>
 
       <div class="p4"><a href="img/cana-web.png" title="canaconvocation.org :: Website :: Design, HTML &amp; CSS, transitioned to CMS" class="thickbox"></a><p>Web :: HTML &amp; CSS</p></div>
       <div class="p5"><a href="img/follettbound.png" title="Follett Library Resources :: Product Logo" class="thickbox"></a><p>LOGO :: ILLUSTRATION</p></div>
