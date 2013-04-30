094931ad4515cd69b9419446a5934e0fc6c376a5
tsidel <jeana@jeanaclark.org>
2013-04-30 00:04:57 -0400

    added web projects to web

--- web/index.php
+++ web/index.php
@@ -1,11 +1,20 @@
 <?php include('../top.php'); ?>
 
-		<div id="main">
-
-		<h3 style="margin:13px;letter-spacing:1px;">WEBSITES</h3>
+    <div id="main">
+    <h3 style="margin:13px;letter-spacing:1px;">WEBSITES</h3>
 
 
     <dl>
+      <dt><img src="/img/web-GattonUnited.png" alt="Gatton United Capital Campaign" width="600" height="526" /> </dt>
+      <dd><h3>Capital Campaign Fundraising Website - Gatton College of Business and Economics</h3></dd>
+      <dd>Led project to create a website highlighting fundraising efforts for a $65 Million capital campaign. Created content calendar, wireframes, and worked with graphic designer to create look and feel of the site.<br /><br />HTML, CSS, &amp; ASP<br /><br />online: <a href="http://gattonunited.uky.edu">gattonunited.uky.edu</a></dd>
+
+
+      <dt><img src="/img/drupal-signage.png" alt="Gatton Signage Screens" width="600" /> </dt>
+      <dd><h3>Digital Signage - Gatton College of Business and Economics</h3></dd>
+      <dd>Drupal drives the Digital Signage at the Gatton College of Business and Economics. I created a calendar content type, and used various views and taxonomy to deliver the calendar content to 3 tv screens throughout the building. Five content editors were trained update the college calendar through Drupal.<br /><br />Drupal, Theming, Views, HTML &amp; CSS<br /><br />online: <a href="http://gattondev.uky.edu/calendar/signage">gattondev.uky.edu/calendar/signage</a><br /><br /></dd>
+
+
       <dt><img src="img/CANA.png" alt="CANA - Convocation of Anglicans in North America" /> </dt>
       <dd><h3>CANA - Convocation of Anglicans in North America Website</h3></dd>
       <dd>Designed first iteration of the CANA website. Worked with client on content development, design, and production. Hand coded with HTML &amp; CSS.</dd>
