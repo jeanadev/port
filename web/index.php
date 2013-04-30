2cfd5a834e0891ba8fa4abbd6ee5fc53d920d748
tsidel <jeana@jeanaclark.org>
2013-04-30 00:28:45 -0400

    changed dd txt color

--- web/index.php
+++ web/index.php
@@ -10,25 +10,10 @@
       <dd>Led project to create a website highlighting fundraising efforts for a $65 Million capital campaign. Created content calendar, wireframes, and worked with graphic designer to create look and feel of the site.<br /><br />HTML, CSS, &amp; ASP<br /><br />online: <a href="http://gattonunited.uky.edu">gattonunited.uky.edu</a></dd>
 
 
-      <dt><img src="/img/drupal-signage.png" alt="Gatton Signage Screens" width="600" /> </dt>
+      <dt><img src="/img/drupal-signage.png" alt="Gatton Signage Screens" width="600" height="446" /> </dt>
       <dd><h3>Digital Signage - Gatton College of Business and Economics</h3></dd>
       <dd>Drupal drives the Digital Signage at the Gatton College of Business and Economics. I created a calendar content type, and used various views and taxonomy to deliver the calendar content to 3 tv screens throughout the building. Five content editors were trained update the college calendar through Drupal.<br /><br />Drupal, Theming, Views, HTML &amp; CSS<br /><br />online: <a href="http://gattondev.uky.edu/calendar/signage">gattondev.uky.edu/calendar/signage</a><br /><br /></dd>
-
-
-      <dt><img src="img/CANA.png" alt="CANA - Convocation of Anglicans in North America" /> </dt>
-      <dd><h3>CANA - Convocation of Anglicans in North America Website</h3></dd>
-      <dd>Designed first iteration of the CANA website. Worked with client on content development, design, and production. Hand coded with HTML &amp; CSS.</dd>
-
-      <dt><img src="img/FEDG_Townhall.png" alt="FEDG Townhall" /> </dt>
-      <dd><h3>Follett Educational Distribution Group Intranet site</h3></dd>
-      <dd>Developed, managed and designed a large corporation intranet site of over 40 pages using original graphics and handcoded HTML/CSS; worked alongside various stakeholders including vice-presidents, IT staff and copywriters. Internal site visits incresed 300% over a four month period.</dd>
-
-      <dt><img src="img/40Days.png" alt="40 Days of Discernment" /></dt>
-      <dd><h3>40 Days of Discernment Website</h3></dd>
-      <dd>Designed website to match the event's identity. Coded using HTML &amp; CSS. <a href="http://www.40daysofdiscernment.org">www.40daysofdiscernment.org</a></dd>
-
-
-  </dl>
+    </dl>
 
 
 <div style="clear:both;"></div>
