426a578ed56782dff28d7009ff849540d2f13c32
tsidel <jeana@jeanaclark.org>
2013-04-30 23:21:58 -0400

    added responsive viewbook project

--- web/index.php
+++ web/index.php
@@ -13,6 +13,11 @@
       <dt><img src="/img/drupal-signage.png" alt="Gatton Signage Screens" width="600" height="446" /> </dt>
       <dd><h3>Digital Signage - Gatton College of Business and Economics</h3></dd>
       <dd>Drupal drives the Digital Signage at the Gatton College of Business and Economics. I created a calendar content type, and used various views and taxonomy to deliver the calendar content to 3 tv screens throughout the building. Five content editors were trained update the college calendar through Drupal.<br /><br />Drupal, Theming, Views, HTML &amp; CSS<br /><br />online: <a href="http://gattondev.uky.edu/calendar/signage">gattondev.uky.edu/calendar/signage</a><br /><br /></dd>
+
+      <dt><img src="/img/web-GattonViewbook.png" alt="Responsive Viewbook" width="600" height="450" /> </dt>
+      <dd><h3>Viewbook - Gatton College of Business and Economics</h3></dd>
+      <dd>A responsive experiment that took the existing print viewbook for the undergraduate program and made it accessible online.  <br /><br />Responsive web design, HTML, CSS, &amp; ASP<br /><br />online: <a href="http://gatton.uky.edu/openingdoors">gatton.uky.edu/openingdoors</a><br /><br /></dd>
+
     </dl>
 
 
