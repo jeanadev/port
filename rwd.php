5030ad9ed0e29d12b9e3928a4395a63386e12fa0
tsidel <jeana@jeanaclark.org>
2013-05-12 01:27:56 -0400

    started responsive styles

---
+++ rwd.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie ie6 lt-ie10 lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie ie7 lt-ie10 lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie ie8 lt-ie10 lt-ie9" lang="en"> <![endif]-->
+<!--[if IE 9]>    <html class="no-js ie ie9 lt-ie10" lang="en"> <![endif]-->
+<!--[if gt IE 9]> <html class="no-js" lang="en"><![endif]-->
+
+<html> 
+
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>Jeana Clark :: Graphic Designer</title>
+
+    <?php
+        $homepage = "/";
+        $currentpage = $_SERVER['REQUEST_URI'];
+        if($homepage==$currentpage) {  ?>
+    <script type="text/javascript" src="/js/jquery.js"></script>
+    <script type="text/javascript" src="/js/thickbox.js"></script>
+    <link href="/css/thickbox.css" rel="stylesheet" type="text/css" />
+            <?php } ?>
+            
+    <link rel="icon" href="/img/favicon.png" />
+
+    <link rel="stylesheet" media="screen" type="text/css" href="/css/normalize.css" />
+    <link rel="stylesheet" media="screen" type="text/css" href="/css/rwd.css" />
+    
+
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,200,300,500,700' rel='stylesheet' type='text/css'>
+
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+</head>
+
+<body class="wireframe">
+	<header id="header">
+	</header>
+	
+<div id="page">
+	
+
+	<header id="logo">
+		<div>		
+		<a href="/">
+            <img src="/img/jlogo.png" alt="JeanaClarkLogo" width="100" height="140" />
+        </a>
+		
+			<div>  
+				<span class="mediumgreen">Jeana</span>
+				<span class="mediumgreen2">Clark</span>
+			</div>
+		</div>
+
+    <nav id="nav">
+		<ul>
+			<li><a href="/web">Web</a></li>
+			<li><a href="/print">Print</a></li>
+			<li><a href="/logos">Logos</a></li>
+			<li><a href="/resume">Resume</a></li>
+		</ul>
+    </nav>
+
+	</header>
+
+    <section id="highlightedwork">
+    	<header>
+    	    <h2>RECENT WORK</h3>
+    	</header>
+    	
+    	<div>
+        <a href="#">
+			<div class="thumb"><img class="" src="" /></div>
+          <h3>WEB :: HTML, CSS, CLASSIC ASP</h3>
+         </a>
+       </div>
+
+    	<div>
+        <a href="#">
+			<div class="thumb"><img class="" src="" /></div>
+          <h3>LOGO :: ILLUSTRATION</h3>
+        </a>
+        </div>
+    	
+    	<div>
+        <a href="#">
+			<div class="thumb"><img class="" src="" /></div>
+          <h3>Zappos</h3>
+        </a>
+        </div>
+
+    	<div>
+        <a href="#">
+			<div class="thumb"><img class="" src="" /></div>
+          <h3>Zappos</h3>
+        </a>
+        </div>
+
+    	<div>
+        <a href="#">
+			<div class="thumb"><img class="" src="" /></div>
+          <h3>Zappos</h3>
+        </a>
+        </div>
+
+    	<div>
+        <a href="#">
+			<div class="thumb"><img class="" src="" /></div>
+          <h3>Zappos</h3>
+        </a>
+        </div>
+
+    	
+    </section>
+    
+    	
+    <footer id="footer">
+    
+    <div class="contact">
+     <div>graphic design</div>
+     <div>web development</div>
+     <div>content strategy</div>
+     <div><a href="mailto:jeana@jeanaclark.org">jeana@jeanaclark.org</a></div>
+	</div>
+    </footer>		
+</div>
+</body>
+</html>
