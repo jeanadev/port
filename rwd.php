3cf346c7a823915e4a5aa4e5b2e6582eb05e525d
tsidel <jeana@jeanaclark.org>
2013-05-13 20:57:47 -0400

    added html5shiv for ie8, padding to footer

--- rwd.php
+++ rwd.php
@@ -1,9 +1,4 @@
 <!doctype html>
-<!--[if lt IE 7]> <html class="no-js ie ie6 lt-ie10 lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js ie ie7 lt-ie10 lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js ie ie8 lt-ie10 lt-ie9" lang="en"> <![endif]-->
-<!--[if IE 9]>    <html class="no-js ie ie9 lt-ie10" lang="en"> <![endif]-->
-<!--[if gt IE 9]> <html class="no-js" lang="en"><![endif]-->
 
 <html> 
 
@@ -12,22 +7,16 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <title>Jeana Clark :: Graphic Designer</title>
 
-    <?php
-        $homepage = "/";
-        $currentpage = $_SERVER['REQUEST_URI'];
-        if($homepage==$currentpage) {  ?>
-    <script type="text/javascript" src="/js/jquery.js"></script>
-    <script type="text/javascript" src="/js/thickbox.js"></script>
-    <link href="/css/thickbox.css" rel="stylesheet" type="text/css" />
-            <?php } ?>
-            
     <link rel="icon" href="/img/favicon.png" />
 
     <link rel="stylesheet" media="screen" type="text/css" href="/css/normalize.css" />
     <link rel="stylesheet" media="screen" type="text/css" href="/css/rwd.css" />
-    
 
-	<link href='http://fonts.googleapis.com/css?family=Raleway:400,200,300,500,700' rel='stylesheet' type='text/css'>
+    <!--[if lt IE 9]>
+          <script src="dist/html5shiv.js"></script>
+    <![endif]-->
+          
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,200,300,500,700' rel='stylesheet' type='text/css'>
 
     
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -39,32 +28,29 @@
 	</header>
 	
 <div id="page">
-	
 
-	<header id="logo">
-		<div>		
-		<a href="/">
-            <img src="/img/jlogo.png" alt="JeanaClarkLogo" width="100" height="140" />
-        </a>
-		
-			<div>  
-				<span class="mediumgreen">Jeana</span>
-				<span class="mediumgreen2">Clark</span>
-			</div>
-		</div>
+  <header id="logo">
 
-    <nav id="nav">
-		<ul>
-			<li><a href="/web">Web</a></li>
-			<li><a href="/print">Print</a></li>
-			<li><a href="/logos">Logos</a></li>
-			<li><a href="/resume">Resume</a></li>
-		</ul>
-    </nav>
+  <div>
+    <a href="/"><img src="/img/jlogo.png" alt="JeanaClarkLogo" width="100" height="140" /></a>
+    <div>
+      <span class="mediumgreen">Jeana</span>
+      <span class="mediumgreen2">Clark</span>
+    </div>
+  </div>
 
-	</header>
+  <nav id="nav">
+    <ul>
+      <li><a href="/web">Web</a></li>
+      <li><a href="/print">Print</a></li>
+      <li><a href="/logos">Logos</a></li>
+      <li><a href="/resume">Resume</a></li>
+    </ul>
+  </nav>
 
-    <section id="highlightedwork">
+  </header>
+
+  <section id="highlightedwork">
     	<header>
     	    <h2>RECENT WORK</h3>
     	</header>
