34d537d80cd0d1195403d08969f5ec4831f3c626
tsidel <jeana@jeanaclark.org>
2013-05-14 22:38:47 -0400

    added new projects & kristi font

--- inside.php
+++ inside.php
@@ -17,7 +17,7 @@
     <![endif]-->
           
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,200,300,500,700' rel='stylesheet' type='text/css'>
-
+<link href='http://fonts.googleapis.com/css?family=Kristi' rel='stylesheet' type='text/css'>
     
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
@@ -50,10 +50,12 @@
 
   </header>
 
+
+  
   <section id="highlightedwork">
-    	<header>
-    	    <h2>Web Development</h2>
-    	</header>
+   <header>
+        <h2>Web Development</h2>
+  </header>    	
     	
     <div class="work">
 		<span class="company">Gatton College of Business &amp; Economics</span>
@@ -70,9 +72,6 @@
 	</div>
     
   
-  
-  
-  
     <div class="work">
     
 		
@@ -88,6 +87,75 @@
     	<p>Drupal drives the Digital Signage at the Gatton College of Business and Economics. I created a calendar content type, and used various views and taxonomy to deliver the calendar content to 3 tv screens throughout the building. Five content editors were trained update the college calendar through Drupal.</p>
         </div>
 	</div>
+	
+    <div class="work">
+    
+		<span class="company">Gatton College of Business &amp; Economics</span>
+
+	    <h2>Responsive Viewbooks</h2>
+		
+		<img src="/img/web-GattonViewbook.png" alt="Responsive Viewbook" style="max-width:100%;height:auto;" />
+		<div class="description">
+		<div class="responsibilities"><h5>RESPONSIBILITIES</h5>
+		<p>Responsive web design, HTML, CSS, &amp; Classic ASP</p></div>
+		
+    	<p>A responsive experiment that took the existing print viewbook for the undergraduate program and made it accessible online.</p>
+        </div>
+	</div>
+	
+    <div class="work">
+    
+		<span class="company">Open Source</span>
+
+	    <h2>Website: ack!</h2>
+		
+		<img src="/img/web-ack.png" alt="ack" style="max-width:100%;height:auto;" />
+		<div class="description">
+		<div class="responsibilities"><h5>RESPONSIBILITIES</h5>
+		<p>HTML &amp; CSS</p></div>
+		
+    	<p>Worked with a graphic designer and the ack! software creator to redesign beyondgrep.com</p>
+        </div>
+	</div>
+	
+    <div class="work">
+    
+		<span class="company">Gatton College of Business and Economics</span>
+
+	    <h2>Email Marketing Campaigns</h2>
+		
+		<img src="/img/email-GattonGroup.jpg" alt="Email Design" style="max-width:100%;height:auto;" />
+		<div class="description">
+		<div class="responsibilities"><h5>RESPONSIBILITIES</h5>
+		<p>Strategic Planning, HTML &amp; CSS</p></div>
+		
+    	<p>Designed HTML emails for various campus departments. Worked as a team with the IT director and Marketing director to create a student-recruitment funnel system, tracking electronic correspondance with potential students from initial contact through application and acceptance.</p>
+        </div>
+	</div>
+	
+    <div class="work">
+    
+		<span class="company">Follett Educational Distribution Group</span>
+
+	    <h2>Intranet Site</h2>
+		
+		<img src="/img/web-FEDG.png" alt="FEDG Townhall" style="max-width:100%;height:auto;" />
+		<div class="description">
+		<div class="responsibilities"><h5>RESPONSIBILITIES</h5>
+		<p>Content Management, HTML, CSS &amp; PHP</p></div>
+		
+    	<p>Developed, managed and designed a large corporation intranet site of over 40 pages using original graphics and handcoded HTML/CSS; worked alongside various stakeholders including vice-presidents, IT staff and copywriters. Internal site visits increased 300% over a four month period.</p>
+        </div>
+	</div>
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
     </section>
     
