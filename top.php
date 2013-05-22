9152ad58b7f34345a55f63a7c124e1a1411c699e
tsidel <jeana@jeanaclark.org>
2013-05-22 00:05:51 -0400

    updated open graph logo

--- top.php
+++ top.php
@@ -26,7 +26,7 @@
 	<meta property="og:title" content="Jeana Clark :: Graphic Designer"/>
     <meta property="og:type" content="website"/>
     <meta property="og:url" content="http://www.jeanaclark.org/"/>
-    <meta property="og:image" content="http://www.jeanaclark.org/img/jlogo2.png"/>
+    <meta property="og:image" content="http://www.jeanaclark.org/img/jlogo3.png"/>
     <meta property="og:site_name" content="Jeana Clark :: Graphic Designer"/>
     <meta property="og:description"
           content="Creative and innovative graphic designer who collaborates with organizations and companies to help them tell their unique and inspiring story. + 12 years in web development, 14 years in print design + HTML, CSS, PHP, JavaScript, ASP, Responsive Web Design. + Drupal + Wordpress + Graphic design skills + logo and brand development, brand collateral and publication design."/>
