176cdebb93fd05ca0d6cf5eb10e89f5cbfe6c87b
tsidel <jeana@jeanaclark.org>
2013-05-21 22:06:26 -0400

    updated print page

--- print/index.php
+++ print/index.php
@@ -4,13 +4,98 @@
    <header>
         <h2>print design</h2>
   </header>    	
-    	
+ 
+ 
+       <div class="work">
+		<span class="company">BWI Inc.</span>
+
+	    <h2>Magazine: TitleTalk</h2>
+		
+		<img src="/img/BWITitleTalk.png" alt="BWI TitleTalk Magazine" style="max-width:100%;height:auto;" />
+		<div class="description">
+		
+		
+    	<p>Designed new magazine for BWI Inc. Readership of over 10,000 public librarians and associates. Worked closely with writers and marketing department to select graphics and produce magazine. </p>
+       </div>
+	</div>
+	
+	
+	 
+       <div class="work">
+		<span class="company">Asbury Theological Seminary</span>
+
+	    <h2>Magazine: Asbury Herald</h2>
+		
+		<img src="/img/AsburyHerald.png" alt="Asbury Herald" style="max-width:100%;height:auto;" />
+		<div class="description">
+		
+		
+    	<p>A quarterly 16-24 page magazine, reaching over 18,000 alumni and donors. Responsibilities include art direction, design, and production.</p>
+       </div>
+	</div>
+ 
+      <div class="work">
+		<span class="company">Serif Design Group</span>
+
+	    <h2>Prevent Child Abuse of Central Kentucky Annual Report</h2>
+		
+		<img src="/img/print-pcacky.png" style="max-width:100%;height:auto;"  alt="Postcard" />
+		<div class="description">
+		
+		
+    	<p>Laid out annual report for local non-profit organization. Created custom illustrations, charts, and selected photography.</p>
+       </div>
+	</div>
+	 
+      <div class="work">
+		<span class="company">Serif Design Group</span>
+
+	    <h2>University of Kentucky College of Law Postcard</h2>
+		
+		<img src="/img/print-UKLawPostcard.png" style="max-width:100%;height:auto;"  alt="Postcard" />
+		<div class="description">
+		
+		
+    	<p>Created the concept and designed postcard for mailing to Alumni and other colleges.</p>
+       </div>
+	</div>
+	
+ 
+     <div class="work">
+		<span class="company">Serif Design Group</span>
+
+	    <h2>ShieldWorks Identity &amp; Rebranding</h2>
+		
+		<img src="/img/print-ShieldWorks.png" style="max-width:100%;height:auto;"  alt="ShieldWorks" />
+		<div class="description">
+		
+		
+    	<p>Worked on a team to rebrand and rename a local construction company. I designed the logo, stationery set, and brochure.</p>
+       </div>
+	</div>
+       	
+       	
+ <div class="work">
+		<span class="company">Serif Design Group</span>
+
+	    <h2>Party Invitation for Fifth Third Bank</h2>
+		
+		<img src="/img/53Invite.png" style="max-width:100%;height:auto;"  alt="FEDG Stationery" />
+		<div class="description">
+		
+		
+    	<p>Based on football and halloween themes, created a fun party invitation for Fifth Third Bank employees.</p>
+       </div>
+	</div>
+    
+    
+           	
     <div class="work">
 		<span class="company">Follett Educational Distribution Group</span>
 
-	    <h2>Print: Stationery Design</h2>
+	    <h2>Stationery Design</h2>
 		
-		<img src="/img/FEDGStationery.png" alt="FEDG Stationery" />
+		<img src="/img/FEDGStationery.png" style="max-width:100%;height:auto;"  alt="FEDG Stationery" />
 		<div class="description">
 		
 		
